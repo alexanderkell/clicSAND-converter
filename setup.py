d4d609ca49d7062ee6f3a81f73d482864b52390b
Alexander Kell <alexander@kell.es>
2022-01-11 11:42:16 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A python script to convert OSeMOSYS output into a format suitable for Excel visualisation',
+    author='Alexander J. M. Kell',
+    license='',
+)
